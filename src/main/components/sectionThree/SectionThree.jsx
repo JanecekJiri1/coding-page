cbbd5bd2f8a3d625c842bf6d6a0e711ab28e60a6
Jiri Janecek <kleric13@seznam.cz>
2022-08-08 12:30:31 +0200

    add sectionThreeBox and hover

--- src/main/components/sectionThree/SectionThree.jsx
+++ src/main/components/sectionThree/SectionThree.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SectionThreeBoxs from "./SectionThreeBoxs";
 import "./sectionThree.style.css";
 
 function SectionThree(props) {
@@ -6,29 +7,8 @@ function SectionThree(props) {
     <div className="sectionThree">
       <h2 className="click-away--title sectionThree-title">Aktuální nabídka</h2>
       {/* <div className="sectionThree--concept"> */}
-      <ul className="sectionThree--concept--top">
-        <li>Základ</li>
-        <li>Pokročilý</li>
-        <li>Doplňky</li>
-      </ul>
-      <div className="sectionThree--concept--boxs">
-        <div className="sectionThree--box">
-          <h2>19§</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="sectionThree--box">
-          <h2>26§</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="sectionThree--box">
-          <h2>34§</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-        <div className="sectionThree--box">
-          <h2>26§/den</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-      </div>
+      <SectionThreeBoxs />
+
       <p className="sectionThree--concept--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur minus vero recusandae.</p>
     </div>
     // </div>
---
+++ src/main/components/sectionThree/SectionThreeBoxs.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import "./sectionThreeBoxs.style.css";
+function SectionThreeBoxs(props) {
+  return (
+    <div>
+      <ul className="sectionThree--concept--top">
+        <li>Základ</li>
+        <li>Pokročilý</li>
+        <li>Doplňky</li>
+      </ul>
+
+      <div className="sectionThree--concept--boxs active">
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>19§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>Základní nejlevnější</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae quod debitis hic, minima quisquam voluptatibus, sint animi
+                tempore cupiditate dolorum inventore voluptate laborum. Quo, aperiam. Perspiciatis, nisi? Distinctio, voluptatum esse?
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>28§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>Základní pokročilý</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci praesentium rerum</p>
+            </div>
+          </div>
+        </div>
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>36§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>Základní středně pokročilý</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci praesentium rerum Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>30§ / den</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>Celodenní procvičováni</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci repellendus voluptate nam cupiditate odit nisi at magni, illo neque
+                perferendis minus suscipit quis minima?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sectionThree--concept--boxs ">
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>19§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>some text</h3>
+            </div>
+          </div>
+        </div>
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>19§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>some text</h3>
+            </div>
+          </div>
+        </div>
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>19§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>some text</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="sectionThree--box">
+          <div className="sectionThree--box--inner">
+            <div className="sectionThree--box--front">
+              <h3>19§</h3>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="sectionThree--box--back">
+              <h3>some text</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SectionThreeBoxs;
