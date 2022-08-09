7cc13d82b5baf9279b1e64be7a3b30aa732a1357
Jiri Janecek <kleric13@seznam.cz>
2022-08-09 22:02:04 +0200

    deleted Menu.jsx in navigation

--- src/main/components/header/Header.jsx
+++ src/main/components/header/Header.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import "./header.style.css";
 import Navigation from "../navigation/Navigation";
-import Menu from "../navigation/menu/Menu";
 
 function Header(props) {
-  const [showMenu, setShowMenu] = useState(false);
-
   return (
     <header id="header">
-      {showMenu && <Menu setShowMenu={setShowMenu} />}
-
-      <Navigation setShowMenu={setShowMenu} />
+      <Navigation />
       <div className="insideHeader">
         <h1 className="click-away--title">
           Love is <br /> one click away
