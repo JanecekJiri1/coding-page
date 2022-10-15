690b911da934ecc65e3ec8bb13d808a5afaa8588
Jiri Janecek <kleric13@seznam.cz>
2022-10-15 15:52:13 +0200

    section2 video transition text

--- src/main/components/sectionTwoVideo/SectionTwoVideo.jsx
+++ src/main/components/sectionTwoVideo/SectionTwoVideo.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./sectionTwoVideo.style.css";
 import { useTranslation } from "react-i18next";
 
 function SectionTwoVideo(props) {
   const { t } = useTranslation();
+
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowText(true);
+    }, 3000);
+  }, []);
+
   return (
     <div className="sectionTwo" id="section2">
       <video src="https://vjs.zencdn.net/v/oceans.mp4" autoPlay muted loop />
-      <div className="sectionTwo--text">
-        <h2>{t("discover")}</h2>
-        <h3>{t("no regret")}</h3>
-      </div>
+      {showText && (
+        <div className="sectionTwo--text">
+          <h2>{t("discover")}</h2>
+          <h3>{t("no regret")}</h3>
+        </div>
+      )}
     </div>
   );
 }
